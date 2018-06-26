b335d666a320e4d4b3c95297eb9c36bc731365e7
QUINCY HUTCHISON <30450645+nativeB@users.noreply.github.com>
2018-06-26 15:27:29 +0000

    Create index.js
    temp

---
+++ index.js
@@ -0,0 +1,10 @@
+let enjoyhint_instance = new EnjoyHint({});
+var enjoyhint_script_steps = [
+    {'click #first':'click here to begin'}
+]
+
+//set script config
+enjoyhint_instance.set(enjoyhint_script_steps);
+
+//run Enjoyhint script
+enjoyhint_instance.run();
