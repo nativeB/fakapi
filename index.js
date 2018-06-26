856d1dbd1c35fa8db424e6ff71bf884b7ea17d34
QUINCY HUTCHISON <30450645+nativeB@users.noreply.github.com>
2018-06-26 15:33:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-let enjoyhint_instance = new EnjoyHint({});
+//initialize instance
+var enjoyhint_instance = new EnjoyHint({});
+
+//simple config. 
+//Only one step - highlighting(with description) "New" button 
+//hide EnjoyHint after a click on the button.
 var enjoyhint_script_steps = [
-    {'click #first':'click here to begin'}
-]
+  {
+    'click .new_btn' : 'Click the "New" button to start creating your project'
+  }  
+];
 
 //set script config
 enjoyhint_instance.set(enjoyhint_script_steps);
